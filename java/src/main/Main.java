6c891280113389fcd155d5fe8bba08ad05251946
Francesco Malinconico <f.malinconico1@studenti.unisa.it>
2015-10-20 10:16:15 +0200

    Inizializzato il progetto Java

---
+++ java/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author ScumpinatoS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ciao Mondo");
+    }
+    
+}
