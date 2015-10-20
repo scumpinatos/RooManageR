d6f5ce1ae3e50443d1c354188188d2113aeab351
Francesco Malinconico <francesco.malink@gmail.com>
2015-10-20 10:08:42 +0200

    Pulito il file index.php

--- server/index.php
+++ server/index.php
@@ -11,29 +11,3 @@ require_once "entity/Nazionalita.php";
  * Date: 13/10/2015
  * Time: 16:03
  */
-
-$nazManager = new NazionalitaManager();
-
-$anag = new Anagrafica();
-$anag->setCodiceFiscale("MLNFNC93T06F912F");
-$anag->setNome("Francesco");
-$anag->setCognome("Malinconico");
-$anag->setIndirizzo("Via Gerardo D'orsi, Calvanico");
-
-
-echo $nazManager->readAll()[0]->getAbbreviazione();
-
-$anag->setNazionalita($nazManager->readAll()[0]->getAbbreviazione());
-$anag->setNumeroDocumento("AEGiiandoogaayy");
-$anag > setTipoDocumento(2);
-$anag->setTelefono("08999999999");
-
-$anagManager = new AnagraficaManager();
-$anagManager->insert($anag);
-
-$anag->setEmail("giando@gay.com");
-$anagManager->update($anag);
-
-if (!$nazManager->delete($nazManager->readAll()[0])) {
-    echo 'Delete error';
-}
