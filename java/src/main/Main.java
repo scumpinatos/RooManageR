595140db4dda94f89ead306b73d044bd5a18057b
Giandomenico <Giandomenico@Samsung-PC>
2015-10-23 14:20:19 +0200

    Riorganizzazione di tutte le classi 2.1

--- java/src/main/Main.java
+++ java/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import ui.UILogin;
+
 
 /**
  *
@@ -8,6 +10,6 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        
+        new UILogin().setVisible(true);
     }
 }
