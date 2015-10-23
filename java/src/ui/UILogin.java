6e004088d4c88d26b57ac4f57820c16bdb5b191d
Giandomenico <Giandomenico@Samsung-PC>
2015-10-23 14:19:29 +0200

    Riorganizzazione di tutte le classi 2.0

--- java/src/ui/UILogin.java
+++ java/src/ui/UILogin.java
@@ -38,7 +38,6 @@ public class UILogin extends javax.swing.JFrame {
         jTextFieldEmail = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jButtonAccedi = new javax.swing.JButton();
         jButtonRegistrati = new javax.swing.JButton();
         jPasswordField = new javax.swing.JPasswordField();
         jLabel3 = new javax.swing.JLabel();
@@ -54,16 +53,9 @@ public class UILogin extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Email");
+        jLabel1.setText("Codice fiscale");
 
         jLabel2.setText("Password");
-
-        jButtonAccedi.setText("Regisrati");
-        jButtonAccedi.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonAccediActionPerformed(evt);
-            }
-        });
 
         jButtonRegistrati.setText("Accedi");
         jButtonRegistrati.addActionListener(new java.awt.event.ActionListener() {
@@ -76,7 +68,7 @@ public class UILogin extends javax.swing.JFrame {
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("RooManageR");
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/roomanager_logo.png"))); // NOI18N
 
         jLabel4.setFont(new java.awt.Font("Segoe UI Light", 2, 12)); // NOI18N
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -92,8 +84,7 @@ public class UILogin extends javax.swing.JFrame {
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextFieldEmail)
-                            .addComponent(jButtonAccedi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButtonRegistrati, javax.swing.GroupLayout.DEFAULT_SIZE, 280, Short.MAX_VALUE)
+                            .addComponent(jButtonRegistrati, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jPasswordField)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -113,7 +104,7 @@ public class UILogin extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 117, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextFieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -123,17 +114,11 @@ public class UILogin extends javax.swing.JFrame {
                 .addComponent(jPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButtonRegistrati, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButtonAccedi, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(5, 5, 5))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButtonAccediActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAccediActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButtonAccediActionPerformed
 
     private void jButtonRegistratiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonRegistratiActionPerformed
         String email = jTextFieldEmail.getText();
@@ -212,7 +197,6 @@ public class UILogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonAccedi;
     private javax.swing.JButton jButtonRegistrati;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
